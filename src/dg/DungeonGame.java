cf1aabf96a5c00c6aea1249781f499da25872e18
Smeagle <markus.faeth@gmx.net>
2016-03-16 21:51:59 +0100

    Erste Klasse mit "Hello World"
    Der Anfang...

---
+++ src/dg/DungeonGame.java
@@ -0,0 +1,13 @@
+package dg;
+
+/**
+ * Die erste Klasse. Einstiegspunkt.
+ * @author Smeagle
+ */
+public class DungeonGame {
+
+	public static void main(String... args) {
+		System.out.println("Hello world!");
+	}
+	
+}
